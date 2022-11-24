ea0a9237a3f92c7529065c1c7964a2074b26b428
Reza Pahlevi Haikal <eskrimayambawang@gmail.com>
2022-11-24 10:36:43 +0700

    improve create cash in - adding product name on dropdown

--- resources/views/cashIn/create.blade.php
+++ resources/views/cashIn/create.blade.php
@@ -21,7 +21,7 @@
                             <select data-live-search="true" id="inv" class="form-control" data-role="select-dropdown" data-profile="minimal" name="inv_id" value="" selected="">
                                 <option value="">PILIH INVOICE</option>
                                 @foreach ($dataInvoice as $item)
-                                <option value="{{ $item->id }}">{{ $item->inv_number}} - {{$item->getCompany->company_name ?? 'kosong'}}</option>
+                                <option value="{{ $item->id }}">{{ $item->inv_number}} | {{$item->getCompany->company_name ?? 'kosong'}} - {{$item->getProduct->name}}</option>
                                 @endforeach
                             </select>
                         </div>
