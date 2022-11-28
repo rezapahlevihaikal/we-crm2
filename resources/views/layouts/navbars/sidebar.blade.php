b437f90bb7772c9aea569bbc4e87a29100f9c75e
Reza Pahlevi Haikal <eskrimayambawang@gmail.com>
2022-11-28 09:27:57 +0700

    update: routing di sidebar buat ke halaman cash in wkwkw lupa

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -209,7 +209,7 @@
                     <div class="collapse show" id="navbar-examples">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="{{route('tipeCashIn')}}">
                                     {{ __('Tipe Cash In') }}
                                 </a>
                             </li>
